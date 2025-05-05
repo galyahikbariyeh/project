4fc31b220f8edc744ac96482efd67b94f0ce46c3
galyahikbariyeh <ghaliahjehad66@gmail.com>
2025-05-05 21:12:52 +0300

    done

---
+++ client/regisReal.js
@@ -0,0 +1,32 @@
+var  Loginform= document.getElementById('Loginform')
+var API_URL='http://127.0.0.1:5050/api/login'
+Loginform.addEventListener('submit',async function (e) {
+    e.preventDefault()
+    var email=document.getElementById('form3Example3').value
+    var password=document.getElementById('form3Example4').value
+    var user={email,password}
+    console.log(user)
+
+    var response= await fetch(API_URL,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(user),
+    }).then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        if(data.message=='User logged in successfully'){
+            alert('User logged in successfully')
+            sessionStorage.setItem('authToken',data.token)
+            //comuserhome
+             window.location.href='proj.html'
+        }
+        else{
+
+            alert('User not found')
+        }
+    })
+    console.log(response)
+    
+})
